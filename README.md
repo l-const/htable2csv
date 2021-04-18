d9c7119ad4f018e1e7bac63b740c0ec1129c6e18
l-const <const-l@outlook.com>
2021-04-18 21:37:30 +0300

    Add clap.rs, version 0.1.1

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Program accepts an http uri and downloads HTML tables
 contained as .csv files.
 
 
+
+
 ```bash
 cargo run https://www.w3schools.com/html/html_tables.asp
 ```
@@ -21,3 +23,26 @@ cargo install htable2csv
 ```bash
 htable2csv https://www.w3schools.com/html/html_tables.asp
 ```
+Help
+
+```bash
+htable2csv -h
+
+htable2csv 0.1.1
+Kostas L. <konlampro94@gmail.com>
+Scrapes HTML tables from the web!
+
+USAGE:
+    htable2csv <uri> [ARGS]
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+ARGS:
+    <uri>        Http page url to be scraped. (Required)
+    <out_dir>    Relative path for the folder to place the output. (Optional)
+    <prefix>     File prefix for the output files for each table. (Optional)
+
+```
+
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,33 @@
+extern crate clap;
 extern crate isahc;
 extern crate nipper;
 
+use clap::{App, Arg};
 use isahc::prelude::*;
 use nipper::Document;
 use nipper::Selection;
+
 use std::fs::File;
 use std::io::{BufWriter, Write};
 
 type Table = Vec<Vec<String>>;
 
-fn parse_document(doc: Document) {
+#[derive(Debug, Default)]
+struct CliOpts<'cli_input> {
+    page: &'cli_input str,
+    out_dir: &'cli_input str,
+    prefix: &'cli_input str,
+}
+
+
+fn parse_document(doc: Document, cli_opts: CliOpts) {
     let selected_table_iter: Vec<_> = doc.select("table").iter().collect();
     for (pos, item) in selected_table_iter.into_iter().enumerate() {
-        parse_table(pos, item);
+        parse_table(pos, item, &cli_opts);
     }
 }
 
-fn parse_table(pos: usize, sel: Selection) {
+fn parse_table(pos: usize, sel: Selection, cli_opts: &CliOpts) {
     let vec_of_vec: Table;
     let rows = sel.select("tr");
     vec_of_vec = rows
@@ -27,11 +38,11 @@ fn parse_table(pos: usize, sel: Selection) {
                 .collect()
         })
         .collect();
-    write_to_file(pos, vec_of_vec).expect("Error creating file!");
+    write_to_file(pos, vec_of_vec, cli_opts).expect("Error creating file!");
 }
 
-fn write_to_file(pos: usize, table: Table) -> std::io::Result<()> {
-    let filename = format!("out{}.csv", pos);
+fn write_to_file(pos: usize, table: Table, cli_opts: &CliOpts) -> std::io::Result<()> {
+    let filename = format!("{}{}{}.csv", cli_opts.out_dir, cli_opts.prefix , pos);
     let file = File::create(&filename).expect("Couldn't create file!");
     let mut buffer = BufWriter::new(file);
     for row in table {
@@ -51,21 +62,43 @@ fn write_to_file(pos: usize, table: Table) -> std::io::Result<()> {
     Ok(())
 }
 
-fn get_page(page: &str) -> () {
-    let mut response = isahc::get(page).expect("Error in http get");
+fn get_page(cli_opts: CliOpts) {
+    let mut response = isahc::get(cli_opts.page).expect("Error in http get");
     if let Ok(body) = response.text() {
         let document = Document::from(&body);
-        parse_document(document)
+        parse_document(document, cli_opts)
     } else {
         println!("Error geting response body!");
     }
 }
 
 fn main() {
-    let mut args = std::env::args();
-    if let Some(page) = args.nth(1) {
-        get_page(&page);
-    } else {
-        println!("Not enought arguments!");
-    }
+    let matches = App::new("htable2csv")
+        .version("0.1.1")
+        .author("Kostas L. <konlampro94@gmail.com>")
+        .about("Scrapes HTML tables from the web!")
+        .arg(
+            Arg::with_name("uri")
+                .required(true)
+                .takes_value(true)
+                .help("Http page url to be scraped. (Required)"),
+        )
+        .arg(
+            Arg::with_name("out_dir")
+                .takes_value(true)
+                .help("Relative path for the folder to place the output. (Optional)"),
+        )
+        .arg(
+            Arg::with_name("prefix")
+                .takes_value(true)
+                .help("File prefix for the output files for each table. (Optional)"),
+        )
+        .get_matches();
+    // get_page(page);
+    let cli_opts = CliOpts {
+        page : matches.value_of("uri").unwrap(),
+        out_dir : matches.value_of("out_dir").unwrap_or("./"),
+        prefix : matches.value_of("prefix").unwrap_or("out")
+    };
+    get_page(cli_opts);
 }
